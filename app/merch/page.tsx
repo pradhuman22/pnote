e1fe8699ced549586936f5210d45e12baaa03f05
pradhuman22 <shresthapradhuman2020@gmail.com>
2025-08-30 00:47:55 +0900

    add merch page

---
+++ app/merch/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MerchPage = () => {
+  return (
+    <div className="max-w-screen-xl container mx-auto px-4 md:px-16">
+      Coming Soon...
+    </div>
+  );
+};
+
+export default MerchPage;
